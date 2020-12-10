46109909e7ca3c52b3002f4314810ddcab2450ed
Carla Rodríguez <crodriguez42@ucol.mx>
2020-12-10 16:03:35 -0600

    implemented factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-export default class App {}
+export default class App {
+  factorial(numero) {
+    let multiplicacion = 1;
+    for (let i = 1; i <= numero; i++) {
+      multiplicacion = multiplicacion * i;
+    }
+    return multiplicacion;
+  }
+}
 
 let app = new App();
+console.log(app.factorial(5));
