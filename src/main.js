ca47722bdd07dd66b8997663e469bf9ab55d0002
Carla Rodríguez <crodriguez42@ucol.mx>
2020-12-10 16:35:42 -0600

    implemented elevar

--- src/main.js
+++ src/main.js
@@ -34,9 +34,19 @@ export default class App {
 
     return suma;
   }
+
+  elevar(numero, potencia) {
+    let resultado = 1;
+    for (let i = 1; i <= potencia; i++) {
+      resultado = resultado * numero;
+    }
+
+    return resultado;
+  }
 }
 
 let app = new App();
 console.log(app.factorial(5));
 console.log(app.convertirAString(5));
 console.log(app.obtenerDivisibles(6));
+console.log(app.elevar(3, 4));
