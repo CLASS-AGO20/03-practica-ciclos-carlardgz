625e2a92e034f400501b40e4f57a0bb18024913f
Carla Rodríguez <crodriguez42@ucol.mx>
2020-12-10 16:23:32 -0600

    implemented convertirAString

--- src/main.js
+++ src/main.js
@@ -6,7 +6,20 @@ export default class App {
     }
     return multiplicacion;
   }
+
+  convertirAString(numero) {
+    let cadena = "";
+    let i = 1;
+
+    while (i <= numero) {
+      cadena += "*";
+      i++;
+    }
+
+    return cadena;
+  }
 }
 
 let app = new App();
 console.log(app.factorial(5));
+console.log(app.convertirAString(5));
